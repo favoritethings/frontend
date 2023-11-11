3a0b0f655a53d892ea023d122991e1d6ac047810
Jerry <0001401@naver.com>
2023-11-11 13:52:33 +0900

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: "360px",
+        md: "700px",
+        lg: "1200px",
+        xl: "1280px",
+        "2xl": "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
