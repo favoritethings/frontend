a20371433d17db163a02496fac0c01cbfe4de314
Junseo Park <junseo_park@tmax.co.kr>
2024-01-15 20:20:35 +0900

    feat: initialize form component

---
+++ apps/client/src/components/form/form.tsx
@@ -0,0 +1,19 @@
+import Input from '@/components/form/input';
+import Label from '@/components/form/label';
+import MultiCheckbox from '@/components/form/multi-checkbox';
+
+const Form = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+Form.Input = Input;
+
+Form.Label = Label;
+
+Form.MultiCheckbox = MultiCheckbox;
+
+export default Form;
