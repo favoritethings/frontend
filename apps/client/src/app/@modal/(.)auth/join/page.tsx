82a673ac93f1244b0b9c30c6b92f01667c31c220
Junseo Park <junseo_park@tmax.co.kr>
2024-01-18 21:41:38 +0900

    style: join container component assembly

--- apps/client/src/app/@modal/(.)auth/join/page.tsx
+++ apps/client/src/app/@modal/(.)auth/join/page.tsx
@@ -1,9 +1,10 @@
+import JoinContainer from '@/app/auth/join/container';
 import Modal from '@/components/modal';
 
 const JoinModal = () => {
   return (
     <Modal>
-      <h1>Join Modal</h1>
+      <JoinContainer />
     </Modal>
   );
 };
---
+++ apps/client/src/app/auth/join/container.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { useRef } from 'react';
+import Form from '@/components/form';
+import Steps from '@/components/steps';
+
+const JoinContainer = () => {
+  const form = useRef<HTMLFormElement>(null);
+  return (
+    <form ref={form}>
+      <Steps activeStep={0}>
+        {({ onHandleNext }) => {
+          return [
+            <div className="flex flex-col items-center">
+              <div className="pb-[52px] w-[420px] flex flex-col gap-y-3.5">
+                <h1 className="font-bold text-[24px] leading-[160%] tracking-[-0.72px] whitespace-pre-wrap pb-[18px]">
+                  {`하트플레이스 이용에\n필요한 정보를 입력해주세요.`}
+                </h1>
+                <Form.Label text="이름" htmlFor="name">
+                  <Form.Input
+                    id="name"
+                    type="text"
+                    placeholder="실명으로 입력해주세요"
+                    required
+                  />
+                </Form.Label>
+                <Form.Label text="연락처" htmlFor="tel">
+                  <Form.Input
+                    id="tel"
+                    type="tel"
+                    placeholder="010-1234-5678"
+                    required
+                  />
+                </Form.Label>
+                <Form.Label text="패스워드" htmlFor="password">
+                  <Form.Input
+                    id="password"
+                    type="password"
+                    placeholder="최소 8자리 이상/문자, 숫자, 기호 중 2종류 조합 구성"
+                    required
+                  />
+                </Form.Label>
+              </div>
+              <button
+                className="bg-black text-white w-[240px] py-2"
+                onClick={onHandleNext}
+              >
+                모두 입력하고 다음으로
+              </button>
+            </div>,
+            <div className="flex flex-col items-center">
+              <div className="pb-[52px] w-[420px] flex flex-col gap-y-3.5">
+                <h1 className="font-bold text-[24px] leading-[160%] tracking-[-0.72px] whitespace-pre-wrap pb-[18px]">
+                  {`@@@님, 아래 정보를 입력하면\n더 정확한 추천을 받을 수 있습니다.`}
+                </h1>
+                <Form.Label text="닉네임" htmlFor="nickname">
+                  <Form.Input
+                    id="nickname"
+                    type="text"
+                    placeholder="최소 2자 최대 6자"
+                    required
+                  />
+                </Form.Label>
+                <Form.Label text="성별" htmlFor="gender">
+                  <Form.MultiCheckbox
+                    options={[
+                      { name: '남자', value: '남자' },
+                      { name: '여자', value: '여자' },
+                      { name: '그외', value: '그외' },
+                    ]}
+                  />
+                </Form.Label>
+                <Form.Label text="생년월일" htmlFor="date">
+                  <Form.Input
+                    id="date"
+                    type="text"
+                    placeholder="4자리 숫자만 입력해주세요. 예시 : 960313"
+                    required
+                  />
+                </Form.Label>
+              </div>
+              <button
+                className="bg-black text-white w-[240px] py-2"
+                onClick={onHandleNext}
+              >
+                스킵하고 가입 완료하기
+              </button>
+            </div>,
+          ];
+        }}
+      </Steps>
+    </form>
+  );
+};
+
+export default JoinContainer;
