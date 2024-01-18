d3040f789ef16439c60bd161ea64489c79b55b73
Jerry <0001401@naver.com>
2024-01-18 22:23:59 +0900

    style: main banner image add and curation card svg add

--- apps/client/src/components/curation/index.tsx
+++ apps/client/src/components/curation/index.tsx
@@ -1,8 +1,11 @@
+"use client";
 import React from "react";
 import CurationHeader from "./header";
 import CurationCard from "./card";
+import axios from "axios";
 
 const Curation = () => {
+  const data = axios.get("http://125.141.125.14:8080/curation/home");
   return (
     <div className="mx-auto mb-[120px] w-[1200px]">
       <CurationHeader />
