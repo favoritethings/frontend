1b38ffcb822b759174291593407f65804604a072
Jerry <0001401@naver.com>
2024-01-19 22:08:52 +0900

    feat: home curation api data binding

--- apps/client/src/components/curation/index.tsx
+++ apps/client/src/components/curation/index.tsx
@@ -1,15 +1,37 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CurationHeader from "./header";
 import CurationCard from "./card";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { Curation } from "./types";
 
 const Curation = () => {
-  const data = axios.get("http://125.141.125.14:8080/curation/home");
+  const [curationData, setCurationData] = useState<Curation | null>(null);
+
+  useEffect(() => {
+    const fetchCurationData = async () => {
+      try {
+        const response: AxiosResponse<Curation> = await axios.get(
+          "http://125.141.125.14:8080/curation/home",
+        );
+        setCurationData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchCurationData();
+  }, []);
+
   return (
     <div className="mx-auto mb-[120px] w-[1200px]">
-      <CurationHeader />
-      <CurationCard />
+      {curationData?.data.map((prev, i) => {
+        return (
+          <div key={i}>
+            <CurationHeader title={prev.title} />
+            <CurationCard spotList={prev.spotList} />
+          </div>
+        );
+      })}
     </div>
   );
 };
