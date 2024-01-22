69b9250fc3169cd02f1b9d0a899b5934d9509fb3
blan19 <oponize@naver.com>
2024-01-22 20:13:44 +0900

    style: add insufficient style on Multi-Checkbox Component

--- apps/client/src/components/form/multi-checkbox.tsx
+++ apps/client/src/components/form/multi-checkbox.tsx
@@ -1,25 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface MultiCheckboxProps {
   options: {
     name: string;
     value: string;
   }[];
+  callback?: (value: string) => void;
 }
 
-const MultiCheckbox = ({ options }: MultiCheckboxProps) => {
+const MultiCheckbox = (props: MultiCheckboxProps) => {
+  const { options, callback } = props;
   const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (callback) callback(value);
+  }, [value]);
 
   return (
     <div className="flex gap-3">
       {options.map((option) => (
-        <input
-          type="checkbox"
-          key={option.name}
-          value={option.value}
-          checked={value === option.value}
-          onChange={(event) => setValue(event.target.value)}
-        />
+        <>
+          <label
+            className={`flex h-[24px] w-[36px] cursor-pointer items-center justify-center text-xs font-medium leading-[180%] tracking-[-0.36px] ${
+              value === option.value
+                ? 'bg-black text-white'
+                : 'bg-slate-200 text-black'
+            }`}
+            htmlFor={option.name}
+          >
+            {option.name}
+          </label>
+          <input
+            className="hidden"
+            type="checkbox"
+            key={option.name}
+            id={option.name}
+            value={option.value}
+            checked={value === option.value}
+            onChange={(event) => {
+              if (value === event.target.value) {
+                setValue('');
+                return;
+              }
+              setValue(event.target.value);
+            }}
+          />
+        </>
       ))}
     </div>
   );
