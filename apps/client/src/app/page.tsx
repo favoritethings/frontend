9c1fb52279017a8108806567aca72847c685a46c
Jerry <0001401@naver.com>
2024-01-24 12:40:49 +0900

    fix: change api call location and cache settings

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,28 @@
+"use client";
 import Banner from "@/components/banner";
 import Curation from "@/components/curation";
+import axios, { AxiosResponse } from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [curationData, setCurationData] = useState<Curation | null>(null);
+
+  const curations = axios
+    .get<Curation>(`${process.env.NEXT_PUBLIC_SERVICE_API_URL}/curation/home`, {
+      headers: {
+        "Cache-Control": "max-age=3600",
+      },
+    })
+    .then((res: AxiosResponse<Curation>) => setCurationData(res.data))
+    .catch((e) =>
+      // @TODO: 에러처리 어떻게 할 건지 논의 필요
+      console.log(e),
+    );
+
   return (
     <main>
       <Banner />
-      <Curation />
+      <Curation curation={curationData} />
     </main>
   );
 }
--- apps/client/src/components/curation/index.tsx
+++ apps/client/src/components/curation/index.tsx
@@ -1,30 +1,11 @@
-"use client";
-import { useEffect, useState } from "react";
 import CurationHeader from "./header";
 import CurationCard from "./card";
-import axios, { AxiosResponse } from "axios";
 import { Curation } from "./types";
 
-const Curation = () => {
-  const [curationData, setCurationData] = useState<Curation | null>(null);
-
-  useEffect(() => {
-    const fetchCurationData = async () => {
-      try {
-        const response: AxiosResponse<Curation> = await axios.get(
-          `${process.env.NEXT_PUBLIC_SERVICE_API_URL}/curation/home`,
-        );
-        setCurationData(response.data);
-      } catch (error) {
-        console.error("error message:", error);
-      }
-    };
-    fetchCurationData();
-  }, []);
-
+const Curation = ({ curation }: { curation: Curation | null }) => {
   return (
     <div className="mx-auto mb-[120px] w-[1200px]">
-      {curationData?.data.map((prev, i) => {
+      {curation?.data.map((prev, i) => {
         return (
           <div key={i}>
             <CurationHeader title={prev.title} />
